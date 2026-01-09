439bc069b7e44a8a88d6a614c1ca321d3cc9591b
nishant2009ak <nscomputer7676@gmail.com>
2026-01-09 18:08:28 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
+/* ===============================
+   CoreLink Networks - Base JS
+   =============================== */
 
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("CoreLink Networks site loaded");
+});
